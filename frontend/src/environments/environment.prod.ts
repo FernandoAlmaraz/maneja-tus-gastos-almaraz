2a7b8924b8bb0b78a4c272c6c62cfb580e4f6c53
Fernando Almaraz <fernandoalmarazdelaquintana@gmail.com>
2026-03-10 21:10:24 -0400

    test for actions

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
+  // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com hgdfhg 
   apiUrl: 'https://maneja-tus-gastos-api-sjvw.onrender.com',
 };
 
